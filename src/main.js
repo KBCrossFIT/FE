596c0d672b8a3ce30a05f99214ad0758841bb3b1
YJ-saturday <yejanot@naver.com>
2024-09-19 13:09:59 +0900

    Revert "kimyejong_24.09.19S"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import vuetify from './plugins/vuetify';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const app = createApp(App);
 app.use(router);
-app.use(vuetify);
-
 app.mount('#app');
