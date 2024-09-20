b9aff0ec298bb174b34f8a700fe0cf2750102b3b
isj0228 <isjsjd0228@gmail.com>
2024-09-20 14:54:16 +0900

    changes- 9/20

---
+++ Users/student/Desktop/final project 초안/FE-dev/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <DefaultLayout>
+            <router-view />
+        </DefaultLayout>
+    </div>
+</template>
+
+<script>
+import { provide, reactive } from 'vue';
+import DefaultLayout from './components/DefaultLayout.vue';
+
+export default {
+    name: 'App',
+    setup() {
+        // Use reactive to make the cart reactive
+        const cart = reactive([]);
+
+        // Provide the cart to child components
+        provide('cart', cart);
+
+        return {
+            cart,
+        };
+    },
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
