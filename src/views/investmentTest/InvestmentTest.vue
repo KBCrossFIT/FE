f27945a4b79ca7238833b40399e12ae8847d75d5
KIMKANGMIN <rkdals159@naver.com>
2024-09-25 17:29:41 +0900

    modalAdjust

--- src/views/investmentTest/InvestmentTest.vue
+++ src/views/investmentTest/InvestmentTest.vue
@@ -1,22 +1,44 @@
-<template>
-    <div>
-        <h1>투자성향분석 페이지</h1>
-        <v-btn class="InvestmentTest-btn" @click="navigateTo('/investment-test-end'), cancel()">
-            제출하기
-        </v-btn>
-    </div>
-</template>
-
 <script>
+import { questions } from '@/dummyQuestion.js'; // 질문 파일 가져오기
+
 export default {
     name: 'MyInvestmentTest',
-
+    data() {
+        return {
+            currentQuestionIndex: 0,
+        };
+    },
+    computed: {
+        currentQuestion() {
+            return questions[this.currentQuestionIndex];
+        },
+    },
     methods: {
         navigateTo(path) {
             this.$router.push(path);
         },
+        nextQuestion() {
+            if (this.currentQuestionIndex < questions.length - 1) {
+                this.currentQuestionIndex++;
+            } else {
+                this.navigateTo('/investment-test-end');
+            }
+        },
     },
 };
 </script>
+
+<template>
+    <div>
+        <h1>투자성향분석 페이지</h1>
+        <div class="InvestmentTest-query">
+            <p>{{ currentQuestion.question }}</p>
+            <div v-for="(option, index) in currentQuestion.options" :key="index">
+                <p>{{ option }}</p>
+            </div>
+        </div>
+        <v-btn class="InvestmentTest-btn" @click="nextQuestion"> 다음 문제 </v-btn>
+    </div>
+</template>
 
 <style scoped></style>
