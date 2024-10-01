de8ab16c4f7fe3624578fd7be4ce8df721f735d6
isj0228 <isjsjd0228@gmail.com>
2024-10-01 13:28:59 +0900

    회원가입

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userApi = axios.create({
-    baseURL: 'http://localhost:8080/api/signup', // 사용자 API 엔드포인트
+    baseURL: 'http://localhost:8080/api/member', // 사용자 API 엔드포인트
     headers: {
         'Content-Type': 'application/json',
     },
