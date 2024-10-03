348d9ae8621ae27ef05bdf6d19c1e77e2fc4b636
origin <isjsjd0228@gmail.com>
2024-10-03 22:04:31 +0900

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,36 @@
 import axios from 'axios';
-
+import { useAuthStore } from '@/store/auth';
 import router from '@/router';
 
 const instance = axios.create({
     timeout: 5000,
 });
+
+instance.interceptors.request.use(
+    (config) => {
+      // 쿠키에서 Refresh-Token 추출
+      const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+        const [name, value] = cookie.split('=');
+        acc[name] = decodeURIComponent(value); // URL 디코딩
+        return acc;
+      }, {});
+    
+      console.log("@@@@@@@@@@@@@@@@",cookies);
+      const token = cookies['Refresh-Token']; // 쿠키에서 'Refresh-Token' 가져오기
+        console.log("###################",token);      
+      if (token) {
+
+        // 토큰이 있는 경우
+        config.headers['Authorization'] = `Bearer ${token}`;
+        console.log(config.headers.Authorization);
+      }
+      return config;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
 
 instance.interceptors.response.use(
     (response) => {
@@ -19,3 +45,4 @@ instance.interceptors.response.use(
 );
 
 export default instance;
+
