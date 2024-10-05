9f99afeaacb3ae64877814ee833e0bb7b9307166
kkmhome <dtdz1111@gmail.com>
2024-10-05 10:39:48 +0900

    accounthome

--- vuetify-project/src/main.js
+++ vuetify-project/src/main.js
@@ -3,18 +3,18 @@
  *
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
-
+// dd
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-registerPlugins(app)
+registerPlugins(app);
 
-app.mount('#app')
+app.mount("#app");
