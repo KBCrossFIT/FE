86ee3ac1da592d638cbf50378934ed3cdbe48b25
YJ-saturday <yejanot@naver.com>
2024-10-11 15:24:28 +0900

    포트폴리오 상세정보, 메인 css

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -127,4 +127,14 @@ export default {
   flex-direction: column;
   flex-grow: 1;
 }
+
+/* .overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+} */
 </style>
